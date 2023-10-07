7402aefa253989693ab27aa2d23cac4f53e18473
Ready to Die <54882064+Abhid1239@users.noreply.github.com>
2023-10-08 01:03:41 +0530

    Assesment Overview Component

--- src/components/AssesmentOverview/AssesmentOverview.jsx
+++ src/components/AssesmentOverview/AssesmentOverview.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import AssesmentCard from "./../AssesmentCard/AssesmentCard";
+import "./AssesmentOverview.css";
+import { ReactComponent as SearchIcon } from './../../assets/svg/search.svg'
+import { ReactComponent as FilterIcon } from './../../assets/svg/filter.svg'
+import { ReactComponent as ChartIcon } from './../../assets/svg/chart.svg'
+import { ReactComponent as PlusIcon } from './../../assets/svg/plus.svg'
 
 const defaultProp = {
 
@@ -8,14 +13,25 @@ const defaultProp = {
 
 function AssesmentOverview(props) {
     return (
-        <div className='asses_overview'>
+        <div className='my-asses__overview'>
             {/* { Assesment Info} */}
             <div>
-                <div className="asses__header">
-                    <p className="asses__text"></p>
-                    <div className="asses__features"></div>
+                <div className="my-asses__header">
+                    <p className="my-asses__text">My Assesment</p>
+                    <div className="my-asses__features">
+                        <span className='my-asses__features-icon'><SearchIcon /></span>
+                        <span className='my-asses__features-icon'><FilterIcon /></span>
+                        <span className='my-asses__features-icon'><ChartIcon /></span>
+                    </div>
                 </div>
-                {/* { new Assignmnet} */}
+                <div className='my-asses__new-wrapper'>
+                    <div className='my-asses__new-add'>
+                        <PlusIcon />
+                    </div>
+                    <p className="my-asses__new-text">New Assessment</p>
+                    <p className="my-asses__new-info">From here you can add questions of multiple types like MCQs, subjective (text or paragraph)!</p>
+
+                </div>
             </div>
             <AssesmentCard />
             <AssesmentCard />
